651bd36ac2f40fbf7a87069afc9e63d2fdce04c4
Lelik777 <96589141+Lelik777@users.noreply.github.com>
2022-03-19 21:43:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-a = int(input('Введите ваш рост: '))
-x = int(input('Введите ваш вес: '))
-b = input('Введите ваш пол (М/Ж): ')
+# This is a sample Python script.
 
-if b == 'М':
-  weight = a * 0.712 - 60
-  print(weight)
-  print('Ваш вес должен быть скорректирован на ' + str(weight - x) + ' кг')
-elif b == 'Ж':
- weight = a * 0.624 - 48.9
- print(weight)
- print('Ваш вес должен быть скорректирован на ' + str(weight - x) + ' кг')
-else:
- print('FALSE')
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
